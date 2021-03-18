cc2a0610bc7dace998198f89b1e91dae7278559a
Unai Bolivar <ujbolivar@gmail.com>
2021-03-18 23:05:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import moment from 'moment';
+
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+console.log("a new change to see if webpack runs correctly");
+
+let name = "Bob", time = "today";
+
+console.log(`Hello ${name}, how are you ${time}?`);
+
+console.log("new line for watch")
+
+console.log("one last time here");
